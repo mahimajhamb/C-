f6248912f618fb23590383f5ed479e437302e691
mahimajhamb <72460580+mahimajhamb@users.noreply.github.com>
2020-10-06 21:45:16 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            string num=Console.ReadLine();
+            int num1 = Int32.Parse(num);
+            Console.WriteLine(num1);
+            int num2 = Convert.ToInt32(num);
+            Console.WriteLine(num2);
+            string s = "123lk";
+            bool res;
+            int num3;
+            res=int.TryParse(s,out num3);
+            Console.WriteLine(res);
+            Console.WriteLine(num3);
+            Console.ReadLine();
+        }
+    }
+}
